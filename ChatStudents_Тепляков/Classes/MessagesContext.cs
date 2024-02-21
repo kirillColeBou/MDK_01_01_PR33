32e1e4e1200c5f509badb7838e796a34d31fc1c5
subbotin.59 <gsubbotin.2019@gmail.com>
2024-02-21 10:37:19 +0500

    Разработанные контексты данных. Тепляков

---
+++ ChatStudents_Тепляков/Classes/MessagesContext.cs
@@ -0,0 +1,13 @@
+﻿using ChatStudents_Тепляков.Classes.Common;
+using ChatStudents_Тепляков.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatStudents_Тепляков.Classes
+{
+    public class MessagesContext : DbContext
+    {
+        public DbSet<Messages> Messages { get; set; }
+        public MessagesContext() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(Config.config);
+    }
+}
---
+++ ChatStudents_Тепляков/Classes/UsersContext.cs
@@ -0,0 +1,13 @@
+﻿using ChatStudents_Тепляков.Classes.Common;
+using ChatStudents_Тепляков.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatStudents_Тепляков.Classes
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UsersContext() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(Config.config);
+    }
+}
