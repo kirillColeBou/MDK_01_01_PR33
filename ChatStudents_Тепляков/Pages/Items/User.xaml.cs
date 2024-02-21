0ac48928955f66cd32b64933f7da04bdd306d394
kirillColeBou <kirillteplycov888@gmail.com>
2024-02-21 20:02:02 +0500

    Отображение статуса онлайн по пользователя, который в течении 5 минут отправлял кому либо сообщения, при обычном входе, статус онлайн применен не будет!

--- ChatStudents_Тепляков/Pages/Items/User.xaml.cs
+++ ChatStudents_Тепляков/Pages/Items/User.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ChatStudents_Тепляков.Classes.Common;
+﻿using ChatStudents_Тепляков.Classes;
+using ChatStudents_Тепляков.Classes.Common;
 using ChatStudents_Тепляков.Models;
 using System;
 using System.Collections.Generic;
@@ -8,10 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Linq;
 
 namespace ChatStudents_Тепляков.Pages.Items
 {
@@ -20,8 +18,10 @@ namespace ChatStudents_Тепляков.Pages.Items
     /// </summary>
     public partial class User : UserControl
     {
+        MessagesContext messagesContext = new MessagesContext();
         Users user;
         Main main;
+
         public User(Users user, Main main)
         {
             InitializeComponent();
@@ -29,6 +29,15 @@ namespace ChatStudents_Тепляков.Pages.Items
             this.main = main;
             imgUser.Source = BitmapFromArrayByte.LoadImage(user.Photo);
             FIO.Content = user.ToFIO();
+            UserOnline();
+        }
+
+        public void UserOnline()
+        {
+            var lastUserMessage = messagesContext.Messages.Where(x => x.UserTo == MainWindow.Instance.LoginUser.Id).OrderByDescending(x => x.DateSending).FirstOrDefault();
+            if (lastUserMessage != null)
+                if (lastUserMessage.DateSending.AddMinutes(5) >= DateTime.Now)
+                    imgOnline.Visibility = Visibility.Visible;
         }
 
         private void SelectChat(object sender, MouseButtonEventArgs e) => main.SelectUser(user);
