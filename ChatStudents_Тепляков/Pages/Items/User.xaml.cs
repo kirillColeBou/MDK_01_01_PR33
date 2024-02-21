b24b727200b69510896114daf5b3f22a9b166ea5
kirillColeBou <kirillteplycov888@gmail.com>
2024-02-21 18:31:52 +0500

    Разработанный пользовательский элемент User. Тепляков

---
+++ ChatStudents_Тепляков/Pages/Items/User.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ChatStudents_Тепляков.Classes.Common;
+using ChatStudents_Тепляков.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatStudents_Тепляков.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для User.xaml
+    /// </summary>
+    public partial class User : UserControl
+    {
+        Users user;
+        Main main;
+        public User(Users user, Main main)
+        {
+            InitializeComponent();
+            this.user = user;
+            this.main = main;
+            imgUser.Source = BitmapFromArrayByte.LoadImage(user.Photo);
+            FIO.Content = user.ToFIO();
+        }
+
+        private void SelectChat(object sender, MouseButtonEventArgs e) => main.SelectUser(user);
+    }
+}
