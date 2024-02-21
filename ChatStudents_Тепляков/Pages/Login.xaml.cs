d05068e09ad8e8b8103fb3e0bfc76855e1cf5493
subbotin.59 <gsubbotin.2019@gmail.com>
2024-02-21 11:09:35 +0500

    Программирование окна Login. Тепляков

---
+++ ChatStudents_Тепляков/Pages/Login.xaml.cs
@@ -0,0 +1,82 @@
+﻿using ChatStudents_Тепляков.Classes;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.IO;
+using Microsoft.Win32;
+using System.Text.RegularExpressions;
+using System.Linq;
+using ChatStudents_Тепляков.Models;
+
+namespace ChatStudents_Тепляков.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public string srcUserImage = "";
+        UsersContext usersContext = new UsersContext();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        public bool CheckEmpty(string Pattern, string Input)
+        {
+            Match m = Regex.Match(Input, Pattern);
+            return m.Success;
+        }
+
+        private void SelectPhoto(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Выберите фотографию:";
+            openFileDialog.InitialDirectory = @"C:\";
+            openFileDialog.Filter = "JPG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|All Files (*.*)|*.*";
+            if(openFileDialog.ShowDialog() == true)
+            {
+                imgUser.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                srcUserImage = openFileDialog.FileName;
+            }
+        }
+
+        private void Continue(object sender, RoutedEventArgs e)
+        {
+            if (!CheckEmpty("^[А-ЯёЁ][а-яА-ЯёЁ]*$", Lastname.Text))
+            {
+                MessageBox.Show("Укажите фамилию!");
+                return;
+            }
+            if(!CheckEmpty("^[А-ЯёЁ][а-яА-ЯёЁ]*$", Firstname.Text))
+            {
+                MessageBox.Show("Укажите имя!");
+                return;
+            }
+            if (!CheckEmpty("^[А-ЯёЁ][а-яА-ЯёЁ]*$", Surname.Text))
+            {
+                MessageBox.Show("Укажите отчество!");
+                return;
+            }
+            if (String.IsNullOrEmpty(srcUserImage))
+            {
+                MessageBox.Show("Выберите фотографию!");
+                return;
+            }
+            if (usersContext.Users.Where(x => x.Firstname == Firstname.Text && x.Lastname == Lastname.Text && x.Surname == Surname.Text).Count() > 0)
+            {
+                MainWindow.Instance.LoginUser = usersContext.Users.Where(x => x.Firstname == Firstname.Text && x.Lastname == Lastname.Text && x.Surname == Surname.Text).First();
+                MainWindow.Instance.LoginUser.Photo = File.ReadAllBytes(srcUserImage);
+                usersContext.SaveChanges();
+            }
+            else
+            {
+                usersContext.Users.Add(new Users(Lastname.Text, Firstname.Text, Surname.Text, File.ReadAllBytes(srcUserImage)));
+                usersContext.SaveChanges();
+                MainWindow.Instance.LoginUser = usersContext.Users.Where(x => x.Firstname == Firstname.Text && x.Lastname == Lastname.Text && x.Surname == Surname.Text).First();
+            }
+            MainWindow.Instance.OpenPages(new Pages.Main());
+        }
+    }
+}
