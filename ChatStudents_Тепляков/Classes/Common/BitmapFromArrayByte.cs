140cf63fb121f90a1faf9c82f74743e5ac081431
subbotin.59 <gsubbotin.2019@gmail.com>
2024-02-21 10:32:16 +0500

    Разработанные общие классы. Тепляков

---
+++ ChatStudents_Тепляков/Classes/Common/BitmapFromArrayByte.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace ChatStudents_Тепляков.Classes.Common
+{
+    public class BitmapFromArrayByte
+    {
+        public static BitmapImage LoadImage(byte[] imageData)
+        {
+            BitmapImage Image = new BitmapImage();
+            using (var Stream = new MemoryStream(imageData))
+            {
+                Stream.Position = 0;
+                Image.BeginInit();
+                Image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                Image.CacheOption = BitmapCacheOption.OnLoad;
+                Image.UriSource = null;
+                Image.StreamSource = Stream;
+                Image.EndInit();
+            }
+            Image.Freeze();
+            return Image;
+        }
+    }
+}
---
+++ ChatStudents_Тепляков/Classes/Common/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatStudents_Тепляков.Classes.Common
+{
+    public class Config
+    {
+        public static readonly string config = "Serverstudent.permaviat.ru;Trusted_Connection=False;Database=pr33;User=sa;Pwd=Asdfg123;";
+    }
+}
